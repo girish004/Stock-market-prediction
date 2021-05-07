2ad08dcd559589f486af14bbbf29e07b92318ab3
girish004 <sabharigirish004@gmail.com>
2021-05-08 01:16:10 +0530

    95% completed

--- Stock/forms.py
+++ Stock/forms.py
@@ -1,4 +1,7 @@
 from django import forms  
 class StudentForm(forms.Form):  
-    firstname = forms.CharField(label="Enter first name",max_length=50)  
-    lastname  = forms.CharField(label="Enter last name", max_length = 100)
+    open = forms.CharField(label="Enter Open value",max_length=50)
+    high  = forms.CharField(label="Enter high value", max_length = 100)
+    low  = forms.CharField(label="Enter low value", max_length = 100)
+    adjacent = forms.CharField(label="Enter Adjacent close value", max_length = 100)
+    vol  = forms.CharField(label="Enter volume", max_length = 100)
